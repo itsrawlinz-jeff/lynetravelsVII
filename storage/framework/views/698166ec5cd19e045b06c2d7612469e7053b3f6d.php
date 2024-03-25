6fb98742daff839d6456769a5b82f72412d96b29
Jeffrey Rowlings <rawlinzjeff264@gmail.com>
2024-03-25 13:09:35 +0300

    Git Push

---
+++ storage/framework/views/698166ec5cd19e045b06c2d7612469e7053b3f6d.php
@@ -0,0 +1,2 @@
+<?php echo $__env->make('Review::frontend.form', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<?php /**PATH C:\xampp\htdocs\LYNE\themes/Mytravel/Car/Views/frontend/layouts/details/review.blade.php ENDPATH**/ ?>
---
+++ themes/Base/ThemeProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace Themes\Base;
+
+
+use Modules\Theme\Abstracts\AbstractThemeProvider;
+
+class ThemeProvider extends AbstractThemeProvider
+{
+
+    public static $version = '';
+    public static $name = 'LYNE TRAVELS';
+    public static function info()
+    {
+        // TODO: Implement info() method.
+    }
+}
